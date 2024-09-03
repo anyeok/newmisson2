dcbe5c4a1671dedcfafa1898c0cc1b77f00bf799
anyeok <https://github.com/anyeok>
2024-09-03 13:21:37 +0900

    misson 2 complete

---
+++ src/main/java/org/example/Request.java
@@ -0,0 +1,41 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Request {
+    private final String actionCode;
+    private final Map<String, String> params = new HashMap<>();
+
+    public Request(String command) {
+        String[] commandList = command.split("\\?", 2);
+
+        actionCode = commandList[0];
+
+        if (commandList.length == 1) return;
+
+        String[] paramsList = commandList[1].split("&");
+
+        ArrayList<String> paramsId = new ArrayList<>();
+
+        for (String paramsRow : paramsList) {
+            String[] paramsStr = paramsRow.split("=", 2);
+            String key = paramsStr[0];
+            if (paramsStr[1].startsWith("&")) {
+                paramsId.add(paramsStr[Integer.parseInt(paramsRow) - 1]);
+            } else {
+                String value = paramsStr[1];
+                params.put(key, value);
+            }
+        }
+    }
+
+    public String getActionCode() {
+        return actionCode;
+    }
+
+    public String getParams(String key) {
+        return params.get(key);
+    }
+}
